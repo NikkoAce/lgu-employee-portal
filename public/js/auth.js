21bbbc2311326a055e992605a2b03e85e2d6c3eb
NikkoAce <moreno.nikkaoce@gmail.com>
2025-09-22 21:19:47 +0800

    fix

--- public/js/auth.js
+++ public/js/auth.js
@@ -42,11 +42,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const signUpButton = document.getElementById('signUp');
     const signInButton = document.getElementById('signIn');
 
-    if (container && signUpButton && signInButton) {
+    if (container && signUpButton) {
         signUpButton.addEventListener('click', () => {
             container.classList.add('right-panel-active');
         });
-
+    }
+    
+    if (container && signInButton) {
         signInButton.addEventListener('click', () => {
             container.classList.remove('right-panel-active');
         });
